b3324ebf133b7171ee929fe7ca0dd3e085361b44
zatomis <zatomis@list.ru>
2023-09-03 14:31:24 +0300

    Скрипт v 1.1 Коррекция кода

--- parcer.py
+++ parcer.py
@@ -1,21 +1,17 @@
-import os
-from environs import Env
 from bs4 import BeautifulSoup
 import requests
 import argparse
 from urllib.parse import urlparse
-from urllib.parse import urljoin
+import urllib.parse
+from time import sleep
 from pathvalidate import sanitize_filename
 
 
 def check_for_redirect(response):
     if response.history:
-        if response.history[0].status_code in [301, 302, 303, 304]:
-            raise requests.TooManyRedirects
+        if response.history:
+            raise requests.HTTPError
 
-
-def clear_comment(comment):
-    return str(comment).replace('<span class="black">книге:</span>','').replace('<span class="black">','').replace('</span>','').replace('- перейти к книгам этого жанра','').strip()
 
 
 def parse_arguments():
@@ -44,49 +40,46 @@ def parse_book_page(html_content):
     book_author = str(title_tag[1]).replace('\\xa0', ' ').strip()
     img_tag = soup.find('div', class_='bookimage').find('img')['src']
     genre_tag = soup.find('span', class_='d_book').find('a')['title']
-    comments = []
-    comment_tag = soup.find_all('span', class_='black')
-    for comment in comment_tag:
-        if(clear_comment(comment)):
-            comments.append({
-                'text': clear_comment(comment),
-            })
+    comments = [comment.text for comment in soup.select('.texts span')]
 
     serialized_book = {
         "title": sanitize_filename(book_name),
         "author": book_author,
         "comments": comments,
         "image_url": img_tag,
-        "genre": clear_comment(genre_tag),
+        "genre": genre_tag,
     }
-    return serialized_book
+    return serialized_book['title']
 
 
-def download_txt(url_book, path_image='book'):
-    os.makedirs(path_image, exist_ok=True)
+def create_json_for_book(book_id):
+    params = {'id': book_id}
+    encoded = urllib.parse.urlencode(params)
+    url_book = f"https://tululu.org/txt.php?{encoded}"
     response = requests.get(url_book)
     response.raise_for_status()
     try:
         check_for_redirect(response=response)
-        book_id = url_book.split('=')[-1]
-        book_text = response.content
         main_url = f"{str(urlparse(url_book).scheme)}://{str(urlparse(url_book).netloc)}"
         url = f"{main_url}/b{book_id}"
         response = requests.get(url)
         response.raise_for_status()
         print(parse_book_page(response.text))
-        print("")
-
-        return os.path.join(path_image, name_image)
     except requests.TooManyRedirects:
         return ""
 
 
 if __name__ == '__main__':
-    env = Env()
-    env.read_env()
     parsed_arguments = parse_arguments()
-    for book_id in range(parsed_arguments.start_id, parsed_arguments.end_id):
-        download_txt(f"https://tululu.org/txt.php?id={book_id}")
+    current_book_id = parsed_arguments.start_id
+    while (current_book_id <= parsed_arguments.end_id):
+        try:
+            create_json_for_book(current_book_id)
+            current_book_id +=1
+        except requests.exceptions.HTTPError:
+            print(f'Книга с ID {current_book_id} не существует')
+            current_book_id += 1
+        except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
+            sleep(5)
 
 
