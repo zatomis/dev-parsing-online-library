db92118e39f8a404dc4e71fbf45e7bb15c72c5d5
zatomis <zatomis@list.ru>
2023-10-11 08:47:33 +0300

    Добавлены ф-ии скачивания книг и картинок к ним

--- parcer.py
+++ parcer.py
@@ -1,7 +1,11 @@
+import os
+import pathlib
+import sys
+
 from bs4 import BeautifulSoup
 import requests
 import argparse
-from urllib.parse import urlparse
+from urllib.parse import urlsplit, urljoin
 from time import sleep
 from pathvalidate import sanitize_filename
 
@@ -11,10 +15,9 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-
 def parse_arguments():
     parser = argparse.ArgumentParser(
-        description='Парсер библиотеки www.tululu.org'
+        description="Парсер библиотеки www.tululu.org"
     )
     parser.add_argument(
         '--start_id',
@@ -31,6 +34,12 @@ def parse_arguments():
     args = parser.parse_args()
     return args
 
+
+def file_extension(url):
+    path, filename = os.path.split(urlsplit(url).path)
+    return filename
+
+
 def parse_book_page(html_content):
     soup = BeautifulSoup(html_content, 'lxml')
     title_tag = soup.find('td', class_='ow_px_td').find('div').find('h1').text.split('::')
@@ -39,15 +48,18 @@ def parse_book_page(html_content):
     img_tag = soup.find('div', class_='bookimage').find('img')['src']
     genre_tag = soup.find('span', class_='d_book').find('a')['title']
     comments = [comment.text for comment in soup.select('.texts span')]
-
+    book_id = soup.select_one('.r_comm input[name="bookid"]')['value']
     serialized_book = {
+        "id": book_id,
         "title": sanitize_filename(book_name),
         "author": book_author,
         "comments": comments,
-        "image_url": img_tag,
+        "book_image": img_tag,
         "genre": genre_tag,
+        "img_path": os.path.join('img', file_extension(img_tag)),
+        "book_path": os.path.join('books', f'{book_name.strip()}.txt')
     }
-    return serialized_book['title']
+    return serialized_book
 
 
 def get_book_by_id(book_id):
@@ -59,19 +71,49 @@ def get_book_by_id(book_id):
     url = f"https://tululu.org/b{book_id}"
     response = requests.get(url)
     response.raise_for_status()
+    return response.text
+
+
+def download_image(url, book_page):
+    img_url = urljoin(url, book_page['book_image'])
+    folder_name = os.path.join('images', file_extension(img_url))
+    pathlib.Path('images').mkdir(parents=True, exist_ok=True)
+    response = requests.get(img_url)
+    response.raise_for_status()
+    with open(folder_name, 'wb') as file:
+        file.write(response.content)
+
+
+def download_txt(url, book_page):
+    book_name = sanitize_filename(book_page['title']).strip()
+    folder_name = os.path.join('books', f'{book_name}.txt')
+    pathlib.Path('books').mkdir(
+        parents=True,
+        exist_ok=True
+    )
+    params = {'id': book_page['id']}
+    download_url = f'{url}txt.php'
+    response = requests.get(download_url, params=params)
+    response.raise_for_status()
+    check_for_redirect(response)
+    with open(folder_name, 'wb') as file:
+        file.write(response.content)
+
 
 if __name__ == '__main__':
+    url = 'https://tululu.org/'
     parsed_arguments = parse_arguments()
     current_book_id = parsed_arguments.start_id
-    while (current_book_id <= parsed_arguments.end_id):
+    while current_book_id <= parsed_arguments.end_id:
         try:
-            get_book_by_id(current_book_id)
-            current_book_id +=1
+            html_book_content = get_book_by_id(current_book_id)
+            book_info = parse_book_page(html_book_content)
+            download_txt(url, book_info)
+            download_image(url, book_info)
+            current_book_id += 1
         except requests.exceptions.HTTPError:
             print(f'Книга с ID {current_book_id} не существует')
             current_book_id += 1
         except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
             print("Отсутствие соединения, ожидание 5сек...", file=sys.stderr)
             sleep(5)
-
-
